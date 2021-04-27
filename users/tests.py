ed4eeac0329851cae6531f95096b313a062c505f
An Nguyen <ngqanlc1995@gmail.com>
2021-04-27 10:08:46 +0700

    Test Users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserAccountTest(TestCase):
+
+    def test_new_superuser(self):
+        db = get_user_model()
+        super_user = db.objects.create_superuser('test@gmail.com', 'password')
+        self.assertEqual(super_user.email, 'test@gmail.com')
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_active)
+
+    def test_new_user(self):
+        db = get_user_model()
+        user = db.objects.create_user('test@gmail.com', 'password')
+        self.assertEqual(user.email, 'test@gmail.com')
+        self.assertFalse(user.is_superuser)
+        self.assertTrue(user.is_active)
+
+    def test_create_user_fail(self):
+        with self.assertRaises(ValueError):    
+            get_user_model().objects.create_superuser('test', 'password')
